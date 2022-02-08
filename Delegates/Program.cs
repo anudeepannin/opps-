ea191e38c1e42c5ef69032a2450882c40ef0b98e
Thota Anudeep <anudeep.thota@bhavnacorp.com>
2022-02-08 13:18:08 +0530

    Delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate void MyDelegate(int a, int b);
+
+    public class Eventclass
+    {
+        public event MyDelegate MyEvent;
+
+        public void RaiseEvent(int a, int b)
+        {
+            MyEvent(a, b);
+            Console.WriteLine("Event Perfomed");
+        }
+
+        public void Add(int x, int y)
+        {
+            Console.WriteLine("Add Method {0}", x + y);
+        }
+
+        public void Subtract(int x, int y)
+        {
+            Console.WriteLine("Subtract Method {0}", x - y);
+        }
+
+        static void Main(string[] args)
+        {
+
+            Eventclass e = new Eventclass();
+            e.MyEvent += new MyDelegate(e.Add);
+            e.MyEvent += new MyDelegate(e.Subtract);
+            e.RaiseEvent(20, 10);
+            Console.ReadLine();
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
